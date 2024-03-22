e37bc4b0dc7e2cac8b1995faa81c6d0915cfb132
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-03-21 22:53:12 -0600

    Creacion store

---
+++ springboot-microservice-store/src/main/java/com/microservice/store/SpringbootMicroserviceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceStoreApplication.class, args);
+	}
+
+}
