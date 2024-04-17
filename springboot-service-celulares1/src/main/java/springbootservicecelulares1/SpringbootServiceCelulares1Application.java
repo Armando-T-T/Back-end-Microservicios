d93f65938422641acc7e81af05c6408a160066d4
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-04-16 23:01:19 -0600

    Celulares1

---
+++ springboot-service-celulares1/src/main/java/springbootservicecelulares1/SpringbootServiceCelulares1Application.java
@@ -0,0 +1,13 @@
+package springbootservicecelulares1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceCelulares1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceCelulares1Application.class, args);
+	}
+
+}
