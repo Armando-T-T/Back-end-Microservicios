1973e357a1c47796a35817cd4de67fb964f1e197
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-04-16 23:55:36 -0600

    Nuevo Constructor

--- springboot-microservice-store/src/main/java/com/microservice/store/controller/StoreController.java
+++ springboot-microservice-store/src/main/java/com/microservice/store/controller/StoreController.java
@@ -3,12 +3,15 @@ package com.microservice.store.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation. PathVariable;
-import org.springframework.web.bind.annotation. RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.microservice.store.models.Celular;
 import com.microservice.store.models.Store;
 import com.microservice.store.services.StoreService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class StoreController {
@@ -17,11 +20,23 @@ public class StoreController {
 	private StoreService storeService;
 	
 	@GetMapping("/list")
-	public List<Store> list() {
+	public List<Store> list(){
 		return storeService.findAll();
 	}
-	@GetMapping("/store/({id}/cantidad/(cantidad}")
+
+	@HystrixCommand(fallbackMethod="metodoGenerico")
+	@GetMapping("/celular/{id}/cantidad/{cantidad}")
 	public Store details(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return storeService.findById(id, cantidad);
+	}
+	
+	public Store metodoGenerico(Long id, Integer cantidad) {
+		Store store = new Store();
+		Celular cel = new Celular(id, "El celular de Armando.", "Android");
+		
+		store.setCantidad(cantidad);
+		store.setCel(cel);
+		
+		return store;
 	}
 }
