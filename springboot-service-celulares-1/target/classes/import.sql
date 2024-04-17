446f82e814ddfb4e3237d3b784fc7feb60438dbf
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-04-17 14:19:27 -0600

    Reinicio

--- springboot-service-celulares-1/target/classes/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S21', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S22', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S23', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Appple', 'IPhone 14', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'IPhone 15', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi', 'Mi', NOW());
