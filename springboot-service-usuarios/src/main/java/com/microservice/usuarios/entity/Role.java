070bd10319e2444018a50809b0add03049039cbf
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-04-25 22:50:18 -0600

    Relacion ManytoMany en Usuarios y Roles

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Role.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Role.java
@@ -1,12 +1,15 @@
 package com.microservice.usuarios.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,9 +22,12 @@ public class Role implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(unique = true, length = 20)
+	@Column(unique = true, length = 30)
 	private String nombre;
 	
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "roles")
+	private List<Usuario> usuarios;
+
 	public Long getId() {
 		return id;
 	}
