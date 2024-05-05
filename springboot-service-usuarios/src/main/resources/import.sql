bea27d4872fe4dde3fec76d7460ee09f2fbc7680
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-05-04 20:10:48 -0600

    Probando CRUD Usuario

--- springboot-service-usuarios/src/main/resources/import.sql
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavo', '12345', 1, 'Gustavo', 'Rodriguez', 'gustavo@gmail.com');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('nissan', '12345', 1, 'Nissan', 'Gtr', 'nissan@gmail.com');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (1, 1);
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (2, 2);
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (2, 1);
