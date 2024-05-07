a24ed6c5b896834e6507ef7a53ef0e4a5e5728b7
Armando-T-T <torrestrevinoarmando@gmail.com>
2024-05-07 14:58:23 -0600

    Correciones Menores 2

--- springboot-service-usuarios/src/main/resources/import.sql
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavo', '12345', 1, 'Gustavo', 'Rodriguez', 'gustavo@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('nissan', '12345', 1, 'Nissan', 'Gtr', 'nissan@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavo', '$2a$10$0.lap8XjrNtVYykcmuQXguLMNQt/wJOsty3pFDKjrNXhnajl8eXQq', 1, 'Gustavo', 'Rodriguez', 'gustavo@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('nissan', '$2a$10$9vJ427exaGS/rNAPeOLTO.VN5aTGUBdQlRmMkfKbGFZ6LxWaV6OHi', 1, 'Nissan', 'Gtr', 'nissan@gmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
